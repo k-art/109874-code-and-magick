c7af68060c9b68fdeed366b1e32a4719f64f42e4
Artem Kozlov <arte.horn@gmail.com>
2017-02-09 04:41:39 +0100

    add accessibility

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -3,8 +3,14 @@
 window.colorizeElement = function (element, colors, property) {
   var currentColor = element.style[property];
 
-  element.addEventListener('click', function () {
+  function changeColor() {
     currentColor = colors[window.utils.getRandomElementExcept(colors, colors.indexOf(currentColor))];
     element.style[property] = currentColor;
+  }
+  element.addEventListener('click', changeColor);
+  element.addEventListener('keydown', function (event) {
+    if (event.keyCode && event.keyCode === 13) {
+      changeColor();
+    }
   });
 };
