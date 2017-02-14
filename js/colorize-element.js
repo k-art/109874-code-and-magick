6a8169c3aa8ea62316949a8fd1b8ad81ee04db06
Artem Kozlov <arte.horn@gmail.com>
2017-02-14 23:07:07 +0100

    encapsulate colorize-element.js

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,19 +1,21 @@
 'use strict';
 
-window.colorizeElement = function (element, colors, property) {
-  element.style[property] = colors[0];
+window.colorizeElement = (function () {
+  return function (element, colors, property) {
+    element.style[property] = colors[0];
 
-  var currentColor = element.style[property];
+    var currentColor = element.style[property];
 
-  function changeColor() {
-    currentColor = window.utils.getRandomElementExcept(colors, currentColor);
-    element.style[property] = currentColor;
-  }
-
-  element.addEventListener('click', changeColor);
-  element.addEventListener('keydown', function (event) {
-    if (window.utils.isActivateEvent(event)) {
-      changeColor();
+    function changeColor() {
+      currentColor = window.utils.getRandomElementExcept(colors, currentColor);
+      element.style[property] = currentColor;
     }
-  });
-};
+
+    element.addEventListener('click', changeColor);
+    element.addEventListener('keydown', function (event) {
+      if (window.utils.isActivateEvent(event)) {
+        changeColor();
+      }
+    });
+  };
+})();
