60b8591fe33b493e25cc6017fbec08d3e36c97da
Artem Kozlov <arte.horn@gmail.com>
2017-02-23 02:23:10 +0100

    fix render wizard

--- js/render.js
+++ js/render.js
@@ -4,21 +4,18 @@ window.render = (function () {
   var setupWizard = document.querySelector('.setup-wizard');
 
   return function (newWizard) {
+    var customWizardElement = setupWizard.cloneNode(true);
     // console.log(newWizard);
 
-    var customWizardElement = setupWizard.cloneNode(true);
     // console.log(customWizardElement);
-
+    customWizardElement.removeAttribute('class');
+    customWizardElement.setAttribute('width', '50');
+    customWizardElement.setAttribute('height', '70');
     customWizardElement.style.position = 'static';
-    customWizardElement.style.width = 50;
-    customWizardElement.style.height = 50;
     customWizardElement.setAttribute('title', newWizard.name);
 
-    var svgElement = customWizardElement.children[0];
-    // console.log(svgElement);
-
-    var newWizardCoat = svgElement.querySelector('#wizard-coat');
-    var newWizardEyes = svgElement.querySelector('#wizard-coat');
+    var newWizardCoat = customWizardElement.querySelector('#wizard-coat');
+    var newWizardEyes = customWizardElement.querySelector('#wizard-coat');
 
     newWizardCoat.style.fill = newWizard.colorCoat;
     newWizardEyes.style.fill = newWizard.colorEyes;
